74a61e25172edc5dce90a56045cd8ccbbf5a7e45
Zahra <zahrasadeghi003@gmail.com>
2024-05-28 11:49:54 +0430

    Initial Commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        String firsName="Zahra";
+        String lastName="Sadeghi";
+        boolean userActive=true;
+        int age=33;
+        long accountNum= 1254689963;
+        System.out.println(firsName +"\t"+lastName + " your age is " + age
+                + " and your accountNumber is " + "\t" + accountNum);
+
+
+    }
+}
